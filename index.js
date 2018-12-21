d298cb9c521be580dfb269b7b19e09cb1e3059b6
Nicolas Zanotti <nicolas@getyourguide.com>
2018-12-21 17:01:16 +0100

    Clearer var names

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const fs = require('fs')
+const PassThrough = require('stream').PassThrough
 const app = new (require('koa'))()
 const router = require('koa-router')()
 const template = fs.readFileSync('./src/index.html', 'utf-8')
 const rendererOptions = { template }
 const renderer = require('vue-server-renderer').createRenderer(rendererOptions)
 const createApp = require('./src/create-app')
-const PassThrough = require('stream').PassThrough
 
 router.get('/', ctx => {
-  const vm = createApp(ctx)
+  const vueApp = createApp(ctx)
   const renderContext = { title: 'Home' }
 
-  renderer.renderToString(vm, renderContext)
+  renderer.renderToString(vueApp, renderContext)
     .then(html => ctx.body = html)
     .catch(error => ctx.throw(error))
 })
 
 router.get('/stream', ctx => {
-  const vm = createApp(ctx)
+  const vueApp = createApp(ctx)
   const renderContext = { title: 'Stream' }
 
   ctx.type = 'html'
 
   // @see https://github.com/koajs/koa/blob/master/docs/api/response.md#stream
-  ctx.body = renderer.renderToStream(vm, renderContext).on('error', ctx.onerror).pipe(PassThrough())
+  ctx.body = renderer.renderToStream(vueApp, renderContext).on('error', ctx.onerror).pipe(PassThrough())
 })
 
 app.use(async (ctx, next) => {
@@ -35,5 +35,4 @@ app.use(async (ctx, next) => {
   }
 })
 app.use(router.routes())
-
 app.listen(3000, () => console.log('App listening on port 3000'))
