9b4b678b71334f5f64f4b49865b869e4e7954430
Nicolas Zanotti <nicolas@getyourguide.com>
2019-01-04 12:00:19 +0100

    Simplify code for the example

---
+++ README.md
@@ -0,0 +1,8 @@
+# SSR Example
+
+This example shows Vue server-side rendering with Koa and Koa Router.
+
+Using Node.js, run:
+
+	npm install
+	npm start
--- index.js
+++ index.js
@@ -2,11 +2,48 @@ const fs = require('fs')
 const PassThrough = require('stream').PassThrough
 const app = new (require('koa'))()
 const router = require('koa-router')()
-const template = fs.readFileSync('./src/index.html', 'utf-8')
+const Vue = require('vue')
+const template = fs.readFileSync('./index.html', 'utf-8')
 const rendererOptions = { template }
 const renderer = require('vue-server-renderer').createRenderer(rendererOptions)
-const createApp = require('./src/create-app')
 
+/**
+ * Instead of directly creating an app instance, we should expose a factory function
+ * that can be repeatedly executed to create fresh app instances for each request.
+ * @see https://ssr.vuejs.org/guide/structure.html#avoid-stateful-singletons
+*/
+function createApp(ctx) {
+  return new Vue({
+    data: {
+      url: ctx.url
+    },
+    template: `
+      <main id="app">
+        <h1>Hello World</h1>
+        <p>
+          <a v-if="url === '/'" href="/stream">Stream</a>
+          <a v-else href="/">Home</a>
+        </p>
+      </main>
+    `,
+    /*
+      Since there are no dynamic updates, of all the lifecycle hooks,
+      only 'beforeCreate' and 'created' will be called during SSR.
+      @see https://ssr.vuejs.org/guide/universal.html#component-lifecycle-hooks
+    */
+    beforeCreate: function() {
+      console.log('server: beforeCreate method called')
+    },
+    created: function() {
+      console.log('server: created method called')
+    }
+  })
+}
+
+/**
+ * The base route creates a new app instance, renders to a string, and attaches
+ * the output to the response upon completion.
+ */
 router.get('/', ctx => {
   const vueApp = createApp(ctx)
   const renderContext = { title: 'Home' }
@@ -16,6 +53,9 @@ router.get('/', ctx => {
     .catch(error => ctx.throw(error))
 })
 
+/**
+ * The stream route creates a new app instance and renders a stream straight to the response.
+ */
 router.get('/stream', ctx => {
   const vueApp = createApp(ctx)
   const renderContext = { title: 'Stream' }
@@ -31,7 +71,7 @@ app.use(async (ctx, next) => {
   try {
     await next()
   } catch (error) {
-    ctx.body = 'An error occurred'
+    ctx.body = `Error: ${error.message}`
   }
 })
 app.use(router.routes())
